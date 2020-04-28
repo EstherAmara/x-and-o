70964f2307ebe95116b89ced7461d8ffc7df0c8b
estherukandu.bincom <estherukandu.bincom@gmail.com>
2020-04-28 15:44:24 +0100

    Updates

---
+++ precache-manifest.1318f2b9a2b1a6490f6f284f8144b575.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c732f12654f6bc60d6ddb34b6d32fc7",
+    "url": "/x-and-o/index.html"
+  },
+  {
+    "revision": "13803435d67cfe8cb2d8",
+    "url": "/x-and-o/static/css/main.5c53ad24.chunk.css"
+  },
+  {
+    "revision": "714f607a763bf1eee104",
+    "url": "/x-and-o/static/js/2.b8339066.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/x-and-o/static/js/2.b8339066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13803435d67cfe8cb2d8",
+    "url": "/x-and-o/static/js/main.8e107312.chunk.js"
+  },
+  {
+    "revision": "df34ffb5db511b340e38",
+    "url": "/x-and-o/static/js/runtime-main.e9d1392a.js"
+  },
+  {
+    "revision": "9de0d279d11d24a9308cf9af7bde1154",
+    "url": "/x-and-o/static/media/img2.9de0d279.jpg"
+  }
+]);
